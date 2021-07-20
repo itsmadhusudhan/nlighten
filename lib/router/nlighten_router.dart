2f3b96c4cfaa0c9ed24860deafec70c57cb31517
Madhusudhan <madhusudhan1863@gmail.com>
2021-07-20 18:57:54 +0530

    show splash screen on both ios and android

--- lib/router/nlighten_router.dart
+++ lib/router/nlighten_router.dart
@@ -4,7 +4,9 @@ import 'package:nlighten/modules/home/home_page.dart';
 import 'package:nlighten/modules/videolist/video_list_page.dart';
 import 'package:nlighten/modules/videoplayer/video_player_page.dart';
 import 'package:nlighten/router/routes.dart';
+import 'package:nlighten/splash_page.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class NLightenRouter {
   static String initialRoute = "/";
 
@@ -17,23 +19,19 @@ class NLightenRouter {
 
     switch (settings.name) {
       case Routes.root:
+        _builder = const SplashPage();
+        break;
+
+      case Routes.homeRoute:
         _builder = HomePage();
         break;
 
       case Routes.categoryListRoute:
-        _builder = CategoryListPage();
+        _builder = const CategoryListPage();
         break;
 
       case Routes.videoListRoute:
-        final _arguments = settings.arguments as VideoCategoryArgumnets;
-
-        _builder = VideoListPage(
-          id: _arguments.categoryId,
-          title: _arguments.categoryTitle,
-        );
-        break;
-
-      case Routes.videoListRoute:
+        // ignore: cast_nullable_to_non_nullable
         final _arguments = settings.arguments as VideoCategoryArgumnets;
 
         _builder = VideoListPage(
@@ -45,7 +43,7 @@ class NLightenRouter {
       case Routes.videoPlayerRoute:
         _fullscreenDialog = true;
 
-        _builder = VideoPlayerPage();
+        _builder = const VideoPlayerPage();
 
         break;
 
---
+++ lib/splash_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:nlighten/router/routes.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      Future.delayed(const Duration(seconds: 1)).then((value) =>
+          Navigator.of(context).pushReplacementNamed(Routes.homeRoute));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFCCE03),
+      body: Padding(
+        padding: const EdgeInsets.all(100.0),
+        child: Center(
+          child: Image.asset("lib/assets/icon/app-icon.png"),
+        ),
+      ),
+    );
+  }
+}
